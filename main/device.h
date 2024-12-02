b7035c63fb85023d000543ecf38b4207d3eaa907
Clif Bratcher <inno@users.noreply.github.com>
2024-12-02 12:39:36 -0500

    comment cleanup

--- main/device.h
+++ main/device.h
@@ -1,35 +1,17 @@
 #ifndef device_h
 #define device_h
 
-// declare the Oberheim matrix hardware :
 #define MATRIX_DEVICE_A 0 //0x0a // matrix connected to Serial1
 #define MATRIX_DEVICE_B 1 //0x0b // matrix connected to Serial2
 #define MATRIX_DEVICE_C 2 //0x0c // matrix connected to Serial4
 #define MATRIX_DEVICE_D 3 //0x0d // matrix connected to Serial5
-/*
-  #define matrix_model_A matrix_1000
-  #define matrix_model_B matrix_1000
-  #define matrix_model_C MATRIX_6
-  #define matrix_model_D matrix_1000
-*/
-//// status of matrix_model
-//typedef union {
-//  struct {
-//    unsigned ALL: 8;
-//  };
-//  struct {
-//    unsigned A: 1;
-//    unsigned B: 1;
-//    unsigned C: 1;
-//    unsigned D: 1;
-//  };
-//} matrix_model_t;
+
 extern bool matrix_model_A;
 extern bool matrix_model_B;
 extern bool matrix_model_C;
 extern bool matrix_model_D;
 
-#define MATRIX_6 1 // 1 if Matrix6/6R , 0 if Matrix 1000
+#define MATRIX_6 1
 #define MATRIX_1000 0
 
 //device var used in chaosmatrix.c and else
@@ -38,17 +20,13 @@ extern unsigned char device; // set the Matrix Device A, B, C or D regarding i2c
 extern unsigned char router_device; // define IIC port
 
 extern unsigned char device_arp; // set the Matrix Device A, B, C or D regarding i2c output
-extern unsigned char device_seq; //
+extern unsigned char device_seq;
 
-// function to set the device :
 extern void Matrix_Modele_Init(void);
-
 extern void Device_Select(unsigned char pin);
 extern void Show_Selected_Device(unsigned char device);
 extern void Device_Init(unsigned char device);
 extern void Router(unsigned char evnt0, unsigned char evnt1, unsigned char evnt2);
 extern void Router_Arp(unsigned char evnt0, unsigned char evnt1, unsigned char evnt2);
-
-//extern matrix_model_t matrix_model;
 
 #endif
