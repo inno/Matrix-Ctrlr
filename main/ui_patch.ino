e02b78561df6bab28a3ccae6148c0c9773fb421b
Clif Bratcher <inno@users.noreply.github.com>
2024-12-19 23:09:49 -0500

    Cleanup formatting

--- main/ui_patch.ino
+++ main/ui_patch.ino
@@ -1,4 +1,3 @@
-
 #include "chaosmatrix.h"
 #include "softpanel.h"
 #include "lcd.h"
@@ -7,11 +6,6 @@
 #include "device.h"
 #include <EEPROM.h>
 
-/////////////////////////////////////////////////////////////////////////////
-//  Display patch pages, page 1 for bank and program changes,
-//  page 2 for MIDI channel setup, edit buffer init and patch compare//
-// page3 for Bank dump from Matrix 1000
-/////////////////////////////////////////////////////////////////////////////
 void UI_Display_Patch(void)
 {
   switch (SoftPanel.Page)
@@ -23,12 +17,10 @@ void UI_Display_Patch(void)
   }
 }
 
-/////////////////////////////////////////////////////////////////////////////
-//  Send MIDI for the patch pages, display patch name
-/////////////////////////////////////////////////////////////////////////////
 void UI_Handle_Patch(void)
 {
-switch (SoftPanel.Page) {
+  switch (SoftPanel.Page)
+  {
     case SOFT_PAGE1: handle_page_patch_main(); break;
     case SOFT_PAGE2: handle_page_patch_get(); break;
     case SOFT_PAGE3: handle_page_patch_save(); break;
