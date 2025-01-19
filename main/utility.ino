1d59c9831713e6acf7cafd77dda435dd34ed16b0
Clif Bratcher <inno@users.noreply.github.com>
2025-01-19 09:56:24 -0500

    Refactor and simplify various LCD routines

--- main/utility.ino
+++ main/utility.ino
@@ -58,28 +58,20 @@ void DOUT_PinSet_Keypanel(unsigned char key1_led, unsigned char key2_led, unsign
 ////////////////////////////////////////////////////////////////////
 unsigned char DOUT_PinGet(unsigned char pin)
 {
-  // return Button_Pin_Read (pin);
   return SR.Led_Pin_Read(pin);
-  // return 0;
 }
 
 ////////////////////////////////////////////////////////////////////
 void LCD_CursorSet(unsigned char cell)
 {
-  // void LCD_CursorSet(unsigned char pos)
   //sets the text cursor on LCD screen
   //0x00-0x3f: first line
   //0x40-0x7f: second line
 
-  int row, col;
-  if (cell < 0x40) {
-    row = 0;
-    col = cell;
-  } else {
-    row = 1;
-    col = cell - 0x40;
-  }
-  lcd.setCursor(col, row);
+  if (cell < LCD_SECONDROWSTARTPOSITION)
+    lcd.setCursor(cell, 0);
+  else
+    lcd.setCursor(cell - LCD_SECONDROWSTARTPOSITION, 1);
 }
 
 
@@ -92,33 +84,49 @@ void LCD_Clear(void)
 ////////////////////////////////////////////////////////////////////
 void LCD_PrintBCD3(int value)
 {
-  if (value < 100) {
-    lcd.print(" ");
-    if (value < 10)
-      lcd.print(" ");
-    lcd.print(value, DEC);
-  } else {
-    lcd.print(value, DEC);
-  }
+  LCD_PrintBCD(value, 3);
 }
 
 
 ////////////////////////////////////////////////////////////////////
 void LCD_PrintBCD2(int value)
 {
-  if (value < 10) {
+  LCD_PrintBCD(value, 2);
+}
+
+int intlen(int n) {
+    if (n < 0) n = (n == INT8_MIN) ? INT8_MAX : -n;
+    if (n < 10) return 1;
+    if (n < 100) return 2;
+    if (n < 1000) return 3;
+    if (n < 10000) return 4;
+    if (n < 100000) return 5;
+    if (n < 1000000) return 6;
+    if (n < 10000000) return 7;
+    if (n < 100000000) return 8;
+    if (n < 1000000000) return 9;
+    return 10;
+}
+
+
+void LCD_PrintBCD(int value, unsigned int width)
+{
+  for (int i = intlen(value); i < width; i++)
     lcd.print(" ");
-    lcd.print(value, DEC);
-  } else {
-    lcd.print(value, DEC);
-  }
+  lcd.print(value, DEC);
+}
+
+void LCD_PrintBCDPrepend(char *prepend, int value, unsigned int width)
+{
+  lcd.print(prepend);
+  LCD_PrintBCD(value, width);
 }
 
 
 ////////////////////////////////////////////////////////////////////
 void LCD_PrintBCD1(int value)
 {
-  lcd.print(value, DEC);
+  LCD_PrintBCD(value, 1);
 }
 
 
